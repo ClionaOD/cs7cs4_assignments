c6828e2bdf1a7620f9af5229c2a823a0df24810e
ClionaOD <odoherc1@tcd.ie>
2020-10-20 17:54:52 +0100

    part a

--- week_2.py
+++ week_2.py
@@ -22,16 +22,30 @@ y = df.iloc[:,2]
 df_pos = df[df['label'] == 1]
 df_neg = df[df['label'] == -1]
 
-plt.scatter(df_pos.iloc[:,0],df_pos.iloc[:,1], marker='o', color='b', s=12)
-plt.scatter(df_neg.iloc[:,0],df_neg.iloc[:,1], marker='+', color='yellow', s=12)
+plt.scatter(df_pos.iloc[:,0],df_pos.iloc[:,1], marker='o', color='b', s=18)
+plt.scatter(df_neg.iloc[:,0],df_neg.iloc[:,1], marker='+', color='purple', s=18)
 plt.xlabel('X_1')
 plt.ylabel('X_2')
+plt.legend(['target = +1','target = -1'])
 plt.savefig('./data_visualisation.pdf')
+plt.show()
 plt.close()
 
 model = LogisticRegression(penalty='none', solver='lbfgs')
 clf = model.fit(X, y)
 print(f'intercept is : {model.intercept_} \n slope is : {model.coef_}')
+
+# get decision boundary line by rearranging thetaTx = 0
+# theta0 = clf.intercept_
+# theta1 = clf.coef_[0][0]
+# theta2 = clf.coef_[0][1]
+# theta0*x0 + theta1*x1 + theta2*x2 = 0
+# x2 = -theta0(1) - theta1*x1
+
+boundary_intercept = -clf.intercept_ / clf.coef_[0][1]
+boundary_slope = -clf.coef_[0][0] / clf.coef_[0][1]
+boundary_x = X1
+boundary_y = boundary_slope*boundary_x + boundary_intercept
 
 y_pred = clf.predict(X)
 pred_df = pd.DataFrame(columns=['X1','X2','y_pred'])
@@ -41,11 +55,14 @@ pred_df['y_pred'] = y_pred
 pred_pos = pred_df[pred_df['y_pred'] == 1]
 pred_neg = pred_df[pred_df['y_pred'] == -1]
 
-plt.close()
-plt.scatter(df_pos.iloc[:,0], df_pos.iloc[:,1], marker='o', color='b', s=12)
-plt.scatter(df_neg.iloc[:,0], df_neg.iloc[:,1], marker='+', color='yellow', s=12)
-plt.scatter(pred_pos.iloc[:,0], pred_pos.iloc[:,1], marker='o', color='darkorange', s=8)
-plt.scatter(pred_neg.iloc[:,0], pred_neg.iloc[:,1], marker='+', color='purple', s=8)
+fig = plt.figure()
+plt.scatter(df_pos.iloc[:,0], df_pos.iloc[:,1], marker='o', color='b', s=18)
+plt.scatter(df_neg.iloc[:,0], df_neg.iloc[:,1], marker='+', color='purple', s=18)
+plt.scatter(pred_pos.iloc[:,0], pred_pos.iloc[:,1], marker='h', color='darkorange', s=9)
+plt.scatter(pred_neg.iloc[:,0], pred_neg.iloc[:,1], marker='v', color='yellow', s=9)
+plt.plot(boundary_x, boundary_y, linewidth=2)
 plt.xlabel('X_1')
 plt.ylabel('X_2')
+plt.legend(['decision boundary','target = +1','target = -1', 'predicted = +1', 'predicted = -1'])
+plt.show()
 plt.savefig('./vis_with_preds.pdf')
