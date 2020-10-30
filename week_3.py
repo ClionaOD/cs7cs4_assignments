631b17b414250aa5e7b4e79841f23e7badf84583
ClionaOD <odoherc1@tcd.ie>
2020-10-30 13:15:25 +0000

    part ii results

--- week_3.py
+++ week_3.py
@@ -90,7 +90,7 @@ for a in alphas:
     surf = ax.plot_trisurf(Xtest[:,0],Xtest[:,1],ridge_preds, color='palegoldenrod',label='predictions')
     surf._edgecolors2d = surf._edgecolors3d
     surf._facecolors2d = surf._facecolors3d
-    
+
     scat = ax.scatter(X1,X2,y, color='r',label='training points')
 
     ax.set_xlabel('X1')
@@ -101,7 +101,7 @@ for a in alphas:
 
     ax.set_title(f'Ridge: 1/2C = {a}')
     ax.legend()
-    
+
     plt.tight_layout()
     save_path = f'./Ridge_plot_alpha={a}.pdf'
     #plt.savefig(save_path)
@@ -112,10 +112,9 @@ for a in alphas:
 #(a)
 a = 0.5 #C=1 therefore alpha=1/2
 lasso = linear_model.Lasso(alpha=a)
-print(f'====== \n Lasso parameters for alpha={a}: \n intercept: {lasso.intercept_} \n slope: {lasso.coef_}')
 
 folds = [5, 2, 10, 25, 50, 100]
-cv_df = pd.DataFrame(index=['mean error', 'variance'], columns=folds)
+cv_df_k = pd.DataFrame(index=['mean error', 'variance'], columns=folds)
 
 for k in folds:
     kf = KFold(n_splits=k)
@@ -132,12 +131,96 @@ for k in folds:
     mean_err = np.mean(np.array(errors))
     std = np.std(np.array(errors))
     
-    cv_df.loc['mean error',k] = mean_err
-    cv_df.loc['variance',k] = std
+    cv_df_k.loc['mean error',k] = mean_err
+    cv_df_k.loc['variance',k] = std
     print(f'===== \n {k}-fold Cross-Val results: \n mean error = {mean_err} \n standard deviation = {std}')
 
-#TODO: plot the variance and mean
+fig, ax = plt.subplots()
+ax.errorbar(np.array(cv_df_k.columns), np.array(cv_df_k.loc['mean error']), yerr=cv_df_k.loc['variance'])
+ax.set_xlabel('number of folds (k)')
+ax.set_ylabel('mean prediction error')
+#plt.savefig('./kfold_mean_std.pdf')
+#plt.show()
+plt.close()
 
+# (b) - (d)
+def cross_val_C(Xpoly, y, iter_list, regularisation):
+    cv_df = pd.DataFrame(index=['mean error', 'variance'], columns=iter_list)
+    
+    for a in iter_list:
+        if regularisation == 'lasso':
+            model = linear_model.Lasso(alpha=a)
+        elif regularisation == 'ridge':
+            model = linear_model.Ridge(alpha=a)
+        
+        kf = KFold(n_splits=5)
 
+        errors = []
+        for i in range(5):
+            for train, test in kf.split(Xpoly):
+                model =  model.fit(Xpoly[train], y[train])
+                ypred = model.predict(Xpoly[test])
+                
+                error = mean_squared_error(y[test],ypred)
+                errors.append(error)
+        
+        mean_err = np.mean(np.array(errors))
+        std = np.std(np.array(errors))
+        
+        cv_df.loc['mean error',a] = mean_err
+        cv_df.loc['variance',a] = std
+
+    return cv_df
+
+a_lasso_wide = cross_val_C(Xpoly, y, np.geomspace(0.0001,1), 'lasso')
+
+fig, ax = plt.subplots()
+ax.errorbar(np.array(a_lasso_wide.columns), np.array(a_lasso_wide.loc['mean error']), yerr=a_lasso_wide.loc['variance'])
+ax.set_xlabel('value for alpha (1/2C)')
+ax.set_ylabel('mean prediction error')
+ax.set_title('lasso cross val for wide range of alpha')
+#plt.savefig('./cv_lasso_1.pdf')
+#plt.show()
+plt.close()
+
+# based on this plot, narrow the range 
+a_lasso_narrow = cross_val_C(Xpoly, y, np.linspace(0.0001,0.1), 'lasso')
+
+fig, ax = plt.subplots()
+ax.errorbar(np.array(a_lasso_narrow.columns), np.array(a_lasso_narrow.loc['mean error']), yerr=a_lasso_narrow.loc['variance'])
+ax.set_xlabel('value for alpha (1/2C)')
+ax.set_ylabel('mean prediction error')
+ax.set_title('lasso cross val for smaller range of alpha')
+#plt.savefig('./cv_lasso_2.pdf')
+#plt.show()
+plt.close()
+#We get a alpha = 0.01 as a sensible choice, from the cross validation plot.
+
+# repeat for Ridge regression
+    # use a different range, based on results from part (i)
+ridge_range = np.geomspace(0.01,100)
+a_ridge_wide = cross_val_C(Xpoly, y, ridge_range, 'ridge')
+
+fig, ax = plt.subplots()
+ax.errorbar(np.array(a_ridge_wide.columns), np.array(a_ridge_wide.loc['mean error']), yerr=a_ridge_wide.loc['variance'])
+ax.set_xlabel('value for alpha (1/2C)')
+ax.set_ylabel('mean prediction error')
+ax.set_title('ridge cross val over wide range of alpha')
+#plt.savefig('./cv_ridge_1.pdf')
+#plt.show()
+plt.close()
+
+# narrow the range tested after initial inspection
+ridge_range = np.linspace(0.01,10)
+a_ridge_narrow = cross_val_C(Xpoly, y, ridge_range, 'ridge')
+
+fig, ax = plt.subplots()
+ax.errorbar(np.array(a_ridge_narrow.columns), np.array(a_ridge_narrow.loc['mean error']), yerr=a_ridge_narrow.loc['variance'])
+ax.set_xlabel('value for alpha (1/2C)')
+ax.set_ylabel('mean prediction error')
+ax.set_title('ridge cross val over narrow range of alpha')
+#plt.savefig('./cv_ridge_1.pdf')
+#plt.show()
+plt.close()
 
 
