0826e995a53b5e087fa2dfc8d8a6a01ffd4f2a55
ClionaOD <odoherc1@tcd.ie>
2020-12-01 12:52:28 +0000

    use slurm for gpu def

---
+++ submit_job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH --output=/home/clionaodoherty/cs7cs4_assignments/10K.out
+#SBATCH --error=/home/clionaodoherty/cs7cs4_assignments/10K.err
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=12
+
+
+python /home/clionaodoherty/cs7cs4_assignments/week8.py
