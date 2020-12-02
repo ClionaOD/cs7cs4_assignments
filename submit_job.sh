b872a142afa0ccd220aeb0a7aaf71c79c5f83362
ClionaOD <odoherc1@tcd.ie>
2020-12-02 14:02:43 +0000

    4 gpus

--- submit_job.sh
+++ submit_job.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --output=/home/clionaodoherty/cs7cs4_assignments/10K.out
 #SBATCH --error=/home/clionaodoherty/cs7cs4_assignments/10K.err
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=12
 
 
