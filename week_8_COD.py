f5e63532fcb0fbc223e0c4b04848c2c341a9c27c
ClionaOD <odoherc1@tcd.ie>
2020-12-03 12:49:27 +0000

    fix conv to cover entire image

--- week_8_COD.py
+++ week_8_COD.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 def convolve(arr, kernel):
     """
@@ -15,9 +16,9 @@ def convolve(arr, kernel):
     output = np.zeros(((arr.shape[0]-kernel.shape[0])+1, (arr.shape[1]-kernel.shape[1])+1))
     
     k = kernel.shape[0]
-    for i in range(k):
+    for i in range(output.shape[0]):
         sub_arr = arr[i:k+i,:]
-        for j in range(k):
+        for j in range(output.shape[1]):
             filt_arr = sub_arr[:,j:k+j]
 
             val = np.sum(filt_arr * kernel)
@@ -29,7 +30,10 @@ img = Image.open('./imagenet_shark.jpg')
 img = img.resize((200,200))
 rgb = np.array(img.convert('RGB'))
 r = rgb[:,:,0]
-Image.fromarray(np.uint8(r)).show()
+plt.imshow(r)
+plt.savefig('./r_channel.jpg')
+plt.show()
+plt.close()
 
 kernel1 = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
 kernel2 = np.array([[0,-1,0],[-1,8,-1],[0,-1,0]])
@@ -37,7 +41,13 @@ kernel2 = np.array([[0,-1,0],[-1,8,-1],[0,-1,0]])
 output1 = convolve(r, kernel1)
 output2 = convolve(r, kernel2)
 
-print(output1)
-print(output2)
+plt.imshow(output1)
+plt.savefig('./kernel1.jpg')
+plt.show()
+plt.close()
 
+plt.imshow(output2)
+plt.savefig('./kernel2.jpg')
+plt.show()
+plt.close()
 
