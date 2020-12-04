8564314fb19ecc472cae288fdd796d9cdefa30cd
ClionaOD <odoherc1@tcd.ie>
2020-12-04 12:50:59 +0000

    .1

--- submit_job.sh
+++ submit_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --output=/home/clionaodoherty/cs7cs4_assignments/5K_0.00001.out
-#SBATCH --error=/home/clionaodoherty/cs7cs4_assignments/5K_0.00001.err
+#SBATCH --output=/home/clionaodoherty/cs7cs4_assignments/5K_0.1.out
+#SBATCH --error=/home/clionaodoherty/cs7cs4_assignments/5K_0.1.err
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=12
 
